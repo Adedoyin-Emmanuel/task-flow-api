c413e66fc16742ce0cb9516354844cc349513624
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-19 14:58:32 +0100

    feat: updated Token model

--- app/Models/Token.php
+++ app/Models/Token.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Token extends Model
 {
     use HasFactory;
+    use HasUuids;
+
+    protected $fillable = [
+        "user_id",
+         "token",
+         "type",
+         "expires_at"
+    ];
 }
