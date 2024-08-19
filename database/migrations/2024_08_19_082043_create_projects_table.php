c2daba520854b68ce12f1efc8d927c5dfce940c6
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-19 10:21:37 +0100

    feat: updated project model

--- database/migrations/2024_08_19_082043_create_projects_table.php
+++ database/migrations/2024_08_19_082043_create_projects_table.php
@@ -14,10 +14,10 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('name');
-            $table->text('description')->nullable();
+            $table->text('description');
             $table->date('start_date');
             $table->date('end_date');
-            $table->string('status')->nullable();
+            $table->string('status')->defaut("pending");
             $table->timestamps();
         });
     }
