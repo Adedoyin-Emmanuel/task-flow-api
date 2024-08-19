7467df24eb89414abbcd42663e755419f73b3e70
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-19 15:03:01 +0100

    feat: added token migration

--- app/Models/Token.php
+++ app/Models/Token.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Token extends Model
 {
@@ -17,4 +18,9 @@ class Token extends Model
          "type",
          "expires_at"
     ];
+
+
+    public function user(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
 }
---
+++ database/migrations/2024_08_19_135951_create_token_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tokens', function (Blueprint $table) {
+            $table->uuid("id");
+            $table->foreignUuid("user_id")->constrained("users")->onDelete("cascade");
+            $table->string("token");
+            $table->enum("type", ["email_verification", "password_reset"]);
+            $table->timestamp("expires_at");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tokens');
+    }
+};
