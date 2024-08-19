007d746546ad28dbde62c29ffd7bf54fc289322a
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-19 10:53:16 +0100

    fix: removed unused imports

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Task extends Model
 {
