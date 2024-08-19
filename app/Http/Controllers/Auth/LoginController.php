017ab19c1b09faf6098d39633dacdd7656463874
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-19 14:50:14 +0100

    feat: updated login controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,5 +7,31 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+
+
+    public function login(Request $request){
+
+        try{
+
+        $validatedData = $request->validate(
+            [
+                "email" => ["required", "string", "email", "max:255", "unique:users"],
+                "password" => ["required", "string", "min:8", "max:20", "confirmed"],
+            ]
+        );
+
+
+        return response()->json([
+            "success" => true,
+            "message" => "Login successful."
+        ]);
+
+    } catch (\Exception $e) {
+        return response()->json([
+            'success' => false,
+            'message' => $e->getMessage(),
+        ], 400);
+
+    }
+  }
 }
