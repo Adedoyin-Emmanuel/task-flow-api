806ac691e685c7c7fb4a666dda0db9c28fbce535
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-19 10:30:01 +0100

    feat: finished task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,10 +2,35 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
     use HasFactory;
+    use HasUuids;
+
+
+    protected $fillable = [
+
+        "name",
+        "description",
+        "project_id",
+        "start_date",
+        "end_date",
+        "assignee"
+    ];
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'assignee');
+    }
 }
