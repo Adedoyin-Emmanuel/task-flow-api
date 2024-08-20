eefa30142b729dd056b5604eba13cb4adac977d9
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-20 07:03:00 +0100

    feat: added Auth middleware

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -5,6 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
 
 class CheckRole
 {
@@ -13,8 +15,20 @@ class CheckRole
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+   public function handle(Request $request, Closure $next)
     {
+        if (!Auth::check()) {
+            return response()->json(['success' => false, 'message' => 'Unauthorized'], 401);
+        }
+
+        $user = Auth::user();
+
+        // Check if the user has the "Project Manager" role
+        if ($user->role !== 'Project Manager') {
+            return response()->json(['success' => false, 'message' => 'Forbidden'], 403);
+        }
+
         return $next($request);
     }
+
 }
