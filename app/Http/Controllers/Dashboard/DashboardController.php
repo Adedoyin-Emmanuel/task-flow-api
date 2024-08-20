5b83a3af514bb64e083fb2ea2049f69d2735a6bf
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-20 18:00:18 +0100

    feat: finished overview method

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -2,16 +2,55 @@
 
 namespace App\Http\Controllers\Dashboard;
 
-use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repositories\ProjectRepository;
+use App\Repositories\TaskRepository;
+
+
 
 class DashboardController extends Controller
 {
-    //
+    protected $taskRepository;
+    protected $projectRepository;
+
+
+
+    public function __construct(ProjectRepository $projectRepository, TaskRepository $taskRepository)
+    {
+        $this->projectRepository = $projectRepository;
+        $this->taskRepository = $taskRepository;
+    }
 
 
     public function overview(Request $request)
     {
+
+        $projects = $this->projectRepository->getAllProjects();
+        $overview = [];
+
+
+        foreach($projects as $project)
+        {
+            $tasks = $this->taskRepository->getTasksByProjectId($project->id);
+            $totalTasks = $tasks->count();
+            $completedTasks = $tasks->where('status', 'completed')->count();
+            $progress = $totalTasks > 0 ? ($completedTasks / $totalTasks) * 100 : 0;
+
+            $overview[] = [
+                "project" => $project,
+                "total_tasks" => $totalTasks,
+                "completed_tasks" => $completedTasks,
+                "progress" => $progress,
+            ];
+
+        }
+
+        return response()->json([
+            "success" => true,
+            "data" => $overview,
+        ]);
 
     }
 
