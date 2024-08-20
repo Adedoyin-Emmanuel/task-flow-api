9ffeae95f2c8272e73a935e481b0de9c437f6ac5
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-20 19:24:21 +0100

    feat: updated TaskAssignedNotification class

--- app/Mail/TaskAssignedNotification.php
+++ app/Mail/TaskAssignedNotification.php
@@ -1,53 +1,27 @@
 <?php
 
 namespace App\Mail;
-
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
 class TaskAssignedNotification extends Mailable
 {
     use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     */
-    public function __construct()
+    public $task;
+
+    public function __construct($task)
     {
-        //
+        $this->task = $task;
     }
 
-    /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
+    public function build()
     {
-        return new Envelope(
-            subject: 'Task Assigned Notification',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'view.name',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
+        return $this->subject('You have been assigned a new task')
+                    ->view('emails.task-assigned')
+                    ->with('task', $this->task);
     }
 }
+
