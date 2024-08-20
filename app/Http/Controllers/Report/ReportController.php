63e19847ad9bfba164a7913a4c66b0c218381b61
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-20 18:30:18 +0100

    feat: updated generateProjectReport method

--- app/Http/Controllers/Report/ReportController.php
+++ app/Http/Controllers/Report/ReportController.php
@@ -4,21 +4,50 @@ namespace App\Http\Controllers\Report;
 
 use Exception;
 use Illuminate\Http\Request;
+use Barryvdh\DomPDF\Facade\Pdf;
 use App\Http\Controllers\Controller;
-use App\Repositories\ProjectRepository;
 use App\Repositories\TaskRepository;
+use App\Repositories\ProjectRepository;
+
 
 
 
 class ReportController extends Controller
 {
 
+    protected $taskRepository;
+    protected $projectRepository;
 
+
+    public function __construct(ProjectRepository $projectRepository, TaskRepository $taskRepository)
+    {
+        $this->projectRepository = $projectRepository;
+        $this->taskRepository = $taskRepository;
+    }
 
     public function generateProjectReport(Request $request, string $projectId)
     {
 
+        $project = $this->projectRepository->getProjectById($projectId);
+        $tasks =  $this->taskRepository->getTaskById($projectId);
 
+
+        $completedTasks = $tasks->where('status', 'completed')->count();
+        $pendingTasks = $tasks->where('status', 'pending')->count();
+        $overdueTasks = $tasks->where('status', 'overdue')->count();
+
+
+        $data = [
+            'project' => $project,
+            'completedTasks' => $completedTasks,
+            'pendingTasks' => $pendingTasks,
+            'overdueTasks' => $overdueTasks,
+        ];
+
+
+        $pdf = PDF::loadView('reports.project', $data);
+
+        return $pdf->download("project_{$project->id}_report.pdf");
 
     }
 
