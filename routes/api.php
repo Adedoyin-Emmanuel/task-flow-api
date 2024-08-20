c9e5fc928d51c843b953f844c1122bcc63c12176
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-20 16:38:41 +0100

    feat: added task routes

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use App\Http\Controllers\Project\ProjectController;
+use App\Http\Controllers\Task\TaskController;
 
 Route::get("/", function () {
     return response()->json([
@@ -30,5 +31,13 @@ Route::group(['prefix' => 'project', 'middleware' => ['auth']], function () {
     Route::delete('/{id}', [ProjectController::class, 'delete'])->middleware('role:admin');
 });
 
+
+Route::group(['prefix' => 'task', 'middlerware' => ['auth']], function(){
+    Route::post('/', [TaskController::class, 'create'])->middleware('role:project manager, admin');
+    Route::get('/', [TaskController::class, 'getAll']);
+    Route::get('/{id}', [TaskController::class, 'getById']);
+    Route::put('/{id}', [TaskController::class, 'update']);
+    Route::delete('/{id}', [TaskController::class, 'delete'])->middleware('role:project manager, admin');
+});
 
 
