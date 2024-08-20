12b208777126567b5e763a81daec9c024cee150e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-08-20 07:04:42 +0100

    feat: added checkAuth middleware

---
+++ app/Http/Middleware/CheckAuth.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!Auth::check()) {
+            return response()->json(['success' => false, 'message' => 'Unauthorized'], 401);
+        }
+
+        return $next($request);
+    }
+}
